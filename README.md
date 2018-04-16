83d78f28c21f327ac78e8e5cdaa54fb82ad7e0f4
Stefanos Chrs <stefanoschrs@hotmail.com>
2018-04-16 16:30:51 +0200

    Add Script & README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# slack-community
+# Slack Community Helper [![license](https://img.shields.io/github/license/mashape/apistatus.svg?style=flat-square)](https://github.com/VideoBrewer/slack-community/blob/master/LICENSE)
+
+> Invite new members to public Slack community
+
+### Usage
+```
+invite.sh [-h] [-t legacyToken] [-e email] [-f filename]
+usage:
+  -h show this help text
+  -t Slack legacy token (api.slack.com/custom-integrations/legacy-tokens)
+  -e invite by email
+  -f invite multiple emails from file
+```
+
+### Example
+```
+./invite.sh -t ttt -e xxx@yyy.com
+./invite.sh -t ttt -f emails.txt
+```
+
---
+++ invite.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+function invite() {
+  curl \
+    -X POST \
+    "https://slack.com/api/users.admin.invite?token=${token}&email=${1}"
+  printf "\n"
+}
+
+function usage() {
+  printf "${usage}"
+  exit $1 $token
+}
+
+usage="$(basename "$0") [-h] [-e email] [-f filename]
+Invite members to public Slack community
+usage:
+  -h show this help text
+  -t Slack legacy token (api.slack.com/custom-integrations/legacy-tokens)
+  -e invite by email
+  -f invite multiple emails from file
+"
+token=""
+
+while getopts 'ht:e:f:' flag; do
+  case "${flag}" in
+    h) 
+      usage
+      ;;
+    t)
+      token="${OPTARG}"
+      ;;
+    e) 
+      printf "Inviting ${OPTARG}: "
+      invite ${OPTARG}
+      exit
+      ;;
+    f) 
+      [[ "`stat ${OPTARG} 2> /dev/null`" == "" ]] && usage 1
+
+      while read email; do
+        printf "Inviting ${email}: "
+        invite $email
+      done < ${OPTARG}
+      exit
+      ;;
+    *)
+      usage 1
+      ;;
+  esac
+done
